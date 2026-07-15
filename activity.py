5f1ec1685506170548fb6e30cde83579e5f3dd4a
unknown <jishnu4211@gmail.com>
2026-07-15 23:50:02 +0530

    my-activity

--- activity.py
+++ activity.py
@@ -1,24 +1,29 @@
 # List..
-# 1.  
+# 1. create a list of 5 favourite movies and print third movies
+#  
 # movies = [ "Mission impossible" , "Transformers" ,"Darknigt" ,"the dark knight rises" ," Ghost Hunter"]
 # print(movies[2])   
 
-# 2. 
+# 2.  add a new movie to the list amd remove one movie
+
 # movies = [ "Mission impossible" , "Transformers" ,"Darknigt" ,"the dark knight rises" ," Ghost Hunter"]
 # movies.insert(1,"KGF")
 # movies.remove("Transformers")
 # print(movies)
 
-# 3 .
+# 3 . sort the list alphabectically
+
 # movies = [ "Mission impossible" , "Transformers" ,"Darknigt" ,"the dark knight rises" ," Ghost Hunter"]
 # movies.sort()
 # print(movies)
 
-# 4 .
+# 4 . convert the list to a tuple and print it ..
+
 # movies = [ "Mission impossible" , "Transformers" ,"Darknigt" ,"the dark knight rises" ," Ghost Hunter"]
 # print(tuple(movies))
 
-# 5 .
+# 5 . access the first and last element of a tuple
+
 # movies = [ "Mission impossible" , "Transformers" ,"Darknigt" ,"the dark knight rises" ," Ghost Hunter"]
 # print(movies[0])
 # print(movies[4])
@@ -27,18 +32,20 @@
 
 # set and dict ...
 
-# 1 .
+# 1 . create two sets
 
 # numbers= {10 ,20 ,30 , 40, 50}
 # nums={12 ,22 ,32 ,42 , 52} 
 
-# 2 .
+# 2 .add and remove elements from a set
+
 # numbers= {10 ,20 ,30 , 40, 50}
 # numbers.add(70) 
 # print(numbers)
 # numbers.remove(30)
 
-# 3 .
+# 3 . create  a dictionary storing student details
+
 # details = {
 #     "name" : "jishnu" ,
 #     "age"  : "22" ,
@@ -49,6 +56,7 @@
 # print(details.values())
 # print(details.items())
 # print(details["age"])
+# 4.
 # print(details["grade" : update()])
 
 
